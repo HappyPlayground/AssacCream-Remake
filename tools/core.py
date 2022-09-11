f0a70fdd463e2d9053090a7e25ebda1be195d282
SapoKR <phmi1621@naver.com>
2022-09-12 00:41:19 +0900

    maybe working start??

--- tools/core.py
+++ tools/core.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from logger import getLogger
 from tools.db impoet users
 from typing import Union
+from numba import jit
+
 log = getLogger(__name__)
 
 class AssacCore():
@@ -11,13 +13,27 @@ class AssacCore():
         self.client = client
         self.data = {}
     
+
+
     async def getUser(self, user_id: Union[int, discord.Member, discord.User)):
         user_id = user_id if isinstance(user_id, int) else user_id.id
-        user = await users.find_one({"_id":user_id})
+        
+        user = self.data["u_"+str(user_id)] if "u_"+str(user_id) in self.data else (await users.find_one({"_id":user_id}))
+
         return if user is None else pass
-        self.data[user_id] = user
+
+        self.data["u_"+user_id] = user
+
         return user
     
+    async def updateUser(self, data: dict, user_id: Union[int, discord.Member, discord.User]):
+        user_id = user_id if isinstance(user_id, int) else user_id
+        
+        data = await getUser(user_id)
+
+        if "_id" not in data.keys():
+            data["_id"] = user_id
+        
     async def clearData(self):
         for i in self.data.keys:
-            
+            pass
